57eede251ea6e43f8a5238c19117fcaafba4f86d
ksy1128 <ksy1128@echomarketing.co.kr>
2024-12-01 18:25:38 +0900

    add middleware, profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,38 @@
-export default function Profile(){
+import { LOGO } from "@/components/svg";
+import db from "@/lib/db";
+import getSession from "@/lib/session"
+import { notFound, redirect } from "next/navigation";
+
+
+
+async function getUser(){
+    const session = await getSession();
+    if( session.id ){
+        const user = await db.user.findUnique({
+            where: { id: session.id }
+        })
+        if( user ) return user;
+    }
+    notFound();
+}
+
+export default async function Profile(){
+
+    const user = await getUser();
+    const logout = async () => {
+        "use server"
+        const session = await getSession();
+        await session.destroy();
+        redirect('/')
+    }
+
     return(
-        <div>
-            profile
+        <div className="py-11">
+            <LOGO classname="size-14 mb-5"/>
+            <h2 className="leading-tight text-xl mb-5">안녕하세요,<br/>{user?.username}님</h2>
+            <form action={logout}>
+                <button className="underline text-gray-500 text-sm">로그아웃</button>
+            </form>
         </div>
     )
 }
