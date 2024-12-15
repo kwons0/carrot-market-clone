6a483aff95f148cfbf2ff1af5ed52af952511100
ksy1128 <ksy1128@echomarketing.co.kr>
2024-12-15 16:08:25 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            hostname: ""
-          }
-        ]
-      }
 };
 
 export default nextConfig;
