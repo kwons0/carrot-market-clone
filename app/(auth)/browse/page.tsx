df4ee54bd41e34587896482a8dc6431ece0d54bf
ksy1128 <ksy1128@echomarketing.co.kr>
2024-12-15 22:40:46 +0900

    fix main

--- app/(auth)/browse/page.tsx
+++ app/(auth)/browse/page.tsx
@@ -6,17 +6,17 @@ import Link from "next/link";
 export default function Browse() {
 
   return (
-    <div className="relative h-screen *:text-center
+    <div className="relative min-h-screen *:text-center
       bg-[url('/img/asset/login-bg1.jpg')] bg-cover
     ">
       <article className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-center mt-32"><Image src="/img/asset/logo_2x.png" alt="logo" width={250} height={250}/></h1>
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col items-center ">
           <div className="relative mt-32">
             <Link href="/create-account" className="w-[73vw] bg-[#F5F5ED]
               py-2 border-[7px] border-[#FFC602] rounded-full text-lg font-semibold
               flex items-center justify-center
-              drop-shadow-[2px_7px_0_#E2B004]
+              drop-shadow-[2px_7px_0_#E2B004] max-w-96
             ">
               <MOUNTAIN_FLAG_ICON classname="w-7 mr-2"/><span>시작하기</span><RIGHT_ARROW_ICON classname="w-7"/>
             </Link>
